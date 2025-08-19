0065d7f8630598a61b55f34c4b7e426f73a2c689
Md Noman Hossain <mdn9707@gmail.com>
2025-08-19 22:08:38 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,26 @@
+import ContainerWrapper from "@/components/common/ContainerWrapper";
+import Categories from "@/components/home/Categories";
+import Hero from "@/components/home/Hero";
+import HowItWorks from "@/components/home/HowItWorks";
+import ServiceProviders from "@/components/home/ServiceProviders";
+import Testimonials from "@/components/home/Testimonials";
+
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-gradient-to-br from-purple-100 to-blue-100">
+        <ContainerWrapper>
+          <Hero />
+        </ContainerWrapper>
+        <Categories />
+        <ServiceProviders />
+        <ContainerWrapper>
+          <HowItWorks />
+        </ContainerWrapper>
+        <Testimonials />
+      </div>
+    </>
+  );
+}
 
