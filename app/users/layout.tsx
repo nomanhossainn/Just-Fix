03a1a3270f62f8d13d35af37340284873cee844b
Md Noman Hossain <mdn9707@gmail.com>
2025-08-22 03:49:51 +0600

    Update layout.tsx

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,1 +1,24 @@
+import UserSidebar from "@/components/dashboard/shared/UserSidebar";
+import UserTopNavbar from "@/components/dashboard/shared/UserTopNavbar";
+import React from "react";
 
+export default function UserDashboardLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            {/* Sidebar: fixed only on large screens */}
+            <div className="lg:fixed lg:z-50 ">
+                <UserSidebar />
+            </div>
+
+            {/* Main content: pushed right on lg screens */}
+            <div className="lg:pl-52 flex flex-col min-h-screen">
+                <UserTopNavbar />
+                <main className="pl-5 pr-5 xl:pr-20 pt-20 pb-5 flex-1 bg-[#f1f7f9]">{children}</main>
+            </div>
+        </div>
+    );
+}
