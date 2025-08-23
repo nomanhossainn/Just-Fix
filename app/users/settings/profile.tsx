fb24cceb3fd4331db62231a34eb36276bb670e07
Md Noman Hossain <mdn9707@gmail.com>
2025-08-23 22:46:21 +0600

    Add Profile component to user settings page

--- app/users/settings/profile.tsx
+++ app/users/settings/profile.tsx
@@ -1,1 +1,13 @@
 
+import Profile from "@/components/dashboard/user/settings/Profile";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+};
+
+export default page;
