5423844ec2802ba177450e9e8b385a6e6820310b
Md Noman Hossain <mdn9707@gmail.com>
2025-08-25 22:41:55 +0600

    Update page.tsx

--- app/users/settings/security/page.tsx
+++ app/users/settings/security/page.tsx
@@ -1,1 +1,12 @@
+import Security from "@/components/dashboard/user/settings/Security";
+import React from "react";
 
+const page = () => {
+    return (
+        <div>
+            <Security />
+        </div>
+    );
+};
+
+export default page;
