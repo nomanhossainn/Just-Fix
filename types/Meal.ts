1b541dc7e7ce52dcc34234e22876cb61f76210fc
Md Noman Hossain <mdn9707@gmail.com>
2025-09-17 22:06:27 +0600

    Add files via upload

--- types/Meal.ts
+++ types/Meal.ts
@@ -1,1 +1,14 @@
-
+export type Meal = {
+  _id: string;
+  name: string;
+  category?: string;
+  description?: string;
+  type: "BREAKFAST" | "LUNCH" | "DINNER" | "SNACK" | "DESSERT";
+  images: { url: string; altText: string }[];
+  price: number;
+  isAvailable: boolean;
+  isDeleted: boolean;
+  deletedAt?: string;
+  createdAt: string;
+  updatedAt: string;
+};
